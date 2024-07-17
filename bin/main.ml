a6a8c5cd44b90a2e35e1888a79a96af399f917c7
maelhos <52194580+maelhos@users.noreply.github.com>
2024-07-17 23:38:32 +0200

    second example in Q

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,6 @@
 open! OCamLLL.Matrix
 
-module Matrix = MakeMatrix(Stdlib.Float)
+module MatrixFloat = MakeMatrix(Float)
 
 let () =
   let m = [
@@ -9,5 +9,19 @@ let () =
     [| 1. ; 0. ; -2. ; 7. |] ;
     [| 6. ; 2. ; 9. ; -5. |]
   ] in 
-  let res = Matrix.gram_schmidt (m :> Matrix.mt)
-  in print_newline (); Matrix.pp_matrix Format.pp_print_float (List.rev res)
+  let res = MatrixFloat.gram_schmidt (m :> MatrixFloat.mt)
+  in print_newline (); MatrixFloat.pp_matrix Format.pp_print_float (List.rev res)
+
+
+module MatrixQ = MakeMatrix(Q)
+
+let () =
+  let m = [
+    [| 4 ; 1 ; 3 ; -1 |] ;
+    [| 2 ; 1 ; -3 ; 4 |] ;
+    [| 1 ; 0 ; -2 ; 7 |] ;
+    [| 6 ; 2 ; 9 ; -5 |]
+  ] in 
+  let m_c = List.map (fun a -> Array.map (fun b -> Q.of_int b) a) m in
+  let res = MatrixQ.gram_schmidt (m_c :> MatrixQ.mt)
+  in print_newline (); MatrixQ.pp_matrix Q.pp_print (List.rev res)
